f73baf9cb7a3255ed03f007747d3415a982f5cf9
Pete Thorne <pete.thorne@ht2.co.uk>
2015-04-12 16:55:03 +0100

    Playing with grid position

--- Jewel.js
+++ Jewel.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var React = require('react-native');
+var TimerMixin = require('react-timer-mixin');
 
 var {
   StyleSheet,
@@ -38,14 +39,36 @@ function jewelStyle(type) {
     width: 30,
     height: 30,
     borderColor: 'rgba(0,0,0,0.7)',
-    opacity: 0,
   };
 }
 
 var Jewel = React.createClass({
+  mixins: [TimerMixin],
   componentDidMount: function () {
-    Animation.startAnimation(this.refs['this'], 400, 0, 'linear', {opacity: 1});
+    this.setTimeout(
+      () => {
+        Animation.startAnimation(this.refs['this'], 1000, 0, 'easeInOutQuad', {opacity:1});
+        this._calculatePosition();
+      },
+      0
+    );
   },
+  // componentWillUpdate: function () {
+  //   this.setTimeout(
+  //     () => {
+  //       Animation.startAnimation(this.refs['this'], 1000, 0, 'easeInOutQuad', {position:30});
+  //     },
+  //     0
+  //   );
+  // },
+  // componentDidUpdate: function () {
+  //   this.setTimeout(
+  //     () => {
+  //       Animation.startAnimation(this.refs['this'], 10, 0, 'easeInOutQuad', {marginTop:0});
+  //     },
+  //     500
+  //   );
+  // },
   _onPressButton: function() {
     var type = this.props.type;
     var jewels = [];
@@ -54,18 +77,38 @@ var Jewel = React.createClass({
       row: this.props.row,
       column: this.props.column,
       type: this.props.type,
+      position: {
+
+      }
     };
 
     this.props.jewelPressCallback(jewel);
   },
+  _calculatePosition: function() {
+    var pos = [
+      (this.props.column) * 30,
+      (this.props.row) * 30,
+    ];
+
+    if(this.refs['this']){
+      Animation.startAnimation(this.refs['this'], 100, 0, 'easeInOutQuad', {position: pos});
+    }
+  },
   render: function() {
+    // this._calculatePosition();
     return (
-      <View ref='this'>
+      <View ref='this' style={styles.hiddenFirst}>
         <TouchableHighlight onPress={this._onPressButton}>
           <View style={jewelStyle(this.props.type)}></View>
         </TouchableHighlight>
       </View>
     );
+  }
+});
+
+var styles = StyleSheet.create({
+  hiddenFirst: {
+    opacity: 0,
   }
 });
 
